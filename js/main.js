2796317a9ff44cad739feb5baae9c725d87abadd
Risa Fujii <risa@kray.jp>
2019-11-09 11:57:21 +0900

    List favourite foods (34:50)

--- js/main.js
+++ js/main.js
@@ -24,7 +24,17 @@ function renderHTML(data) {
   let htmlString = '';
 
   for (i = 0; i < data.length; i++) {
-    htmlString += `<p>${data[i].name} is a ${data[i].species}.</p>`
+    htmlString += `<p>${data[i].name} is a ${data[i].species} that likes to eat `
+
+    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+      if (ii == 0) {
+        htmlString += data[i].foods.likes[ii];
+      } else {
+        htmlString += ` and ${data[i].foods.likes[ii]}`;
+      }
+
+    }
+    htmlString += '.</p>'
   }
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
